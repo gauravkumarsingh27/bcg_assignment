4d54d144651d5bf0a20931c07fab620d08c9675d
Gaurav <gksingh9186@gmail.com>
2019-08-07 23:39:09 +0530

    s

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+// CODELAB: Update cache names any time any of the cached files change.
+const FILES_TO_CACHE = [
+    '/offline.html',
+  ];
+
+  // CODELAB: Precache static resources here.
+evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[ServiceWorker] Pre-caching offline page');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+);

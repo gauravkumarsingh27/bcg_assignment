da55259d93d8205400a9dd237470eb920d829f33
Gaurav <gksingh9186@gmail.com>
2019-08-08 00:39:06 +0530

    as

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('video-store').then(function(cache) {
       return cache.addAll([
-        '/',
+        '/bcg_assignment',
     
       ]);
     })

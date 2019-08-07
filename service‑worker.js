f5be25891d385805efea6eb3a1d2063a04c88960
Gaurav <gksingh9186@gmail.com>
2019-08-07 21:59:30 +0530

    TEst

--- service‑worker.js
+++ service‑worker.js
@@ -1,22 +1,10 @@
-var cacheName = 'hello-pwa';
-var filesToCache = [
-  '/'
+const FILES_TO_CACHE = [
+  '/offline.html',
 ];
-
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
-});
-
-/* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
-});
+// CODELAB: Precache static resources here.
+evt.waitUntil(
+  caches.open(CACHE_NAME).then((cache) => {
+    console.log('[ServiceWorker] Pre-caching offline page');
+    return cache.addAll(FILES_TO_CACHE);
+  })
+);
